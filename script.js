a20706efcb625db144d883b212bafdc8a3f0df52
suba0107 <suba0107@stud.kea.dk>
2019-05-27 14:36:14 +0200

    Burgermenu færdig uden animation + påbegynde afleveringer

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+window.addEventListener("load", sidenVises);
 
+function sidenVises() {
+    console.log("sidenVises");
+    document.querySelector("#ikon").addEventListener("click", toggleMenu);
+
+}
+
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#ikon").textContent = "☰";
+    } else {
+        document.querySelector("#ikon").textContent = "X";
+    }
+}
